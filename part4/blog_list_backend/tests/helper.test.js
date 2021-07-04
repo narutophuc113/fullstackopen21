3014c566a22b98edaf6c6cef7aa3eba46005241e
narutophuc113 <narutophuc001@gmail.com>
2021-07-04 16:27:51 +0700

    part4 bloglist finish with mostBlog and mostLike

--- part4/blog_list_backend/tests/helper.test.js
+++ part4/blog_list_backend/tests/helper.test.js
@@ -174,4 +174,20 @@ describe('favorite blog',()=>{
             likes: 12
         })
     })
+
+    test('most blog', ()=>{
+        const result= listHelper.mostBlogs(blogs)
+        expect(result).toEqual({
+            author: 'Robert C. Martin',
+            blogs: 3
+        })
+    })
+
+    test('most like', ()=>{
+        const result= listHelper.mostLikes(blogs)
+        expect(result).toEqual({
+            author: 'Edsger W. Dijkstra',
+            likes: 17
+        })
+    })
 })
--- part4/blog_list_backend/utils/list_helper.js
+++ part4/blog_list_backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -9,24 +11,68 @@ const totalLikes = (blogs) => {
     return sum
 }
 
-const favoriteBlog= (blogs)=>{
-    if(blogs.length==0){
+const favoriteBlog = (blogs) => {
+    if (blogs.length == 0) {
         return {}
     }
-    const favorite= blogs.reduce((init, current)=>{
-        if(init.likes < current.likes){
+    const favorite = blogs.reduce((init, current) => {
+        if (init.likes < current.likes) {
             return current
         }
         return init
     })
-    const result={
-        title:favorite.title,
-        author:favorite.author,
+    const result = {
+        title: favorite.title,
+        author: favorite.author,
         likes: favorite.likes
     }
     return result
 }
 
+const mostBlogs = blogs => {
+    if (blogs.length == 0) {
+        return {}
+    }
+
+    const countBlog = blogs.reduce((init, current) => {
+        let index = _.findIndex(init, {author: current.author})
+        if (index === -1) {
+            init.push({
+                author: current.author,
+                blogs: 1
+            })
+        } else {
+            init[index].blogs += 1
+        }
+        return init
+    }, [])
+
+    const result=_.maxBy(countBlog, 'blogs')
+    return result
+}
+
+const mostLikes = blogs => {
+    if (blogs.length == 0) {
+        return {}
+    }
+
+    const countLike = blogs.reduce((init, current) => {
+        let index = _.findIndex(init, {author: current.author})
+        if (index === -1) {
+            init.push({
+                author: current.author,
+                likes: current.likes
+            })
+        } else {
+            init[index].likes += current.likes
+        }
+        return init
+    }, [])
+
+    const result=_.maxBy(countLike, 'likes')
+    return result
+}
+
 module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
